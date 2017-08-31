30b078c461c64abc84d7d2ccb31eda4c13538ec5
Lon Smith <proflon5@gmail.com>
2017-08-31 14:54:50 -0500

    another change

--- HelloCSCI3020/src/HelloCSCI3020.java
+++ HelloCSCI3020/src/HelloCSCI3020.java
@@ -5,6 +5,7 @@ public class HelloCSCI3020
 	public static void main(String[] args)
 	{
 //		This is the top
+//		Class is fun
 		System.out.println("Hello CSCI 3020!");
 		Simple msg = new Simple("Goodbye");
 		System.out.println(msg);
