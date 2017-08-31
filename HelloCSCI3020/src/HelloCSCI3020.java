fdeb2afaad693af754d70ad92b4224392e10e246
Lon Smith <proflon5@gmail.com>
2017-08-31 15:03:56 -0500

    asd

--- HelloCSCI3020/src/HelloCSCI3020.java
+++ HelloCSCI3020/src/HelloCSCI3020.java
@@ -7,6 +7,7 @@ public class HelloCSCI3020
 //		This is the top
 //		This is a new line
 //		Class is fun
+//		A line from new version
 		System.out.println("Hello CSCI 3020!");
 		Simple msg = new Simple("Goodbye");
 		System.out.println(msg);
