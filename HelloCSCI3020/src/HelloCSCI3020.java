978f177a95c83037dd7dffeef0a0e383a0a1c89f
Lon Smith <proflon5@gmail.com>
2017-08-31 14:52:18 -0500

    Copy 1

--- HelloCSCI3020/src/HelloCSCI3020.java
+++ HelloCSCI3020/src/HelloCSCI3020.java
@@ -5,6 +5,7 @@ public class HelloCSCI3020
 	public static void main(String[] args)
 	{
 //		This is the top
+//		This is a new line
 		System.out.println("Hello CSCI 3020!");
 		Simple msg = new Simple("Goodbye");
 		System.out.println(msg);
