20cebf80db5666b5de44d85debc060fbc38166cc
Lon Smith <proflon5@gmail.com>
2017-08-31 14:25:43 -0500

    Showing commit

--- HelloCSCI3020/src/HelloCSCI3020.java
+++ HelloCSCI3020/src/HelloCSCI3020.java
@@ -4,6 +4,7 @@ public class HelloCSCI3020
 
 	public static void main(String[] args)
 	{
+//		This is the top
 		System.out.println("Hello CSCI 3020 ;)!");
 		Simple msg = new Simple("Goodbye");
 		System.out.println(msg);
